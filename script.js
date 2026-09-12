831fc9757a2e54c1b77174bb39e6ff6fab5ad494
jonneschreuder <jonne.gabrielle@live.nl>
2026-09-12 21:28:16 +0200

    Jonne (#16)
    * padding production-section
    
    * navigation-desktop
    
    ---------
    
    Co-authored-by: Jonne Schreuder <jonne.schreuder@anwb.nl>

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 document.getElementById("socials2").innerHTML = document.getElementById("socials").innerHTML;
+
+let activeSection
+
+function onScroll() {
+  console.log(window.scrollY);
+  if(window.scrollY < 780) {
+    activeSection = "home-nav"
+  }
+  if(window.scrollY >= 780 && window.scrollY < 4300) {
+    activeSection = "producties-nav"
+  }
+  if(window.scrollY >= 4300 && window.scrollY < 5600) {
+    activeSection = "over-ons-nav"
+  }
+  if(window.scrollY >= 5600) {
+    activeSection = "steun-ons-nav"
+  }
+  document.getElementsByClassName("navigation-active")[0]?.classList.remove("navigation-active");
+  document.getElementById(activeSection).classList.add("navigation-active");
+};
+
+onScroll();
+
+window.addEventListener('scrollend', onScroll, { passive: true });
